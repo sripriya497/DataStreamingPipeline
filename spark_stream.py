61e928ccc55971642b71f4865d2a967b37173bec
sripriya497 <sripriyadharshini497@gmail.com>
2024-08-13 14:21:02 -0700

    Modified data insertion to cassandra

--- spark_stream.py
+++ spark_stream.py
@@ -34,36 +34,6 @@ def create_table(session):
 
     print("Table created successfully!")
 
-
-def insert_data(session, **kwargs):
-    print("inserting data...")
-
-    user_id = kwargs.get('id')
-    first_name = kwargs.get('first_name')
-    last_name = kwargs.get('last_name')
-    gender = kwargs.get('gender')
-    address = kwargs.get('address')
-    postcode = kwargs.get('postcode')
-    email = kwargs.get('email')
-    username = kwargs.get('username')
-    dob = kwargs.get('dob')
-    registered_date = kwargs.get('registered_date')
-    phone = kwargs.get('phone')
-    picture = kwargs.get('picture')
-
-    try:
-        session.execute("""
-            INSERT INTO spark_streams.created_users(id, first_name, last_name, gender, address, 
-                post_code, email, username, dob, registered_date, phone, picture)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (user_id, first_name, last_name, gender, address,
-              postcode, email, username, dob, registered_date, phone, picture))
-        logging.info(f"Data inserted for {first_name} {last_name}")
-
-    except Exception as e:
-        logging.error(f'could not insert data due to {e}')
-
-
 def create_spark_connection():
     s_conn = None
 
